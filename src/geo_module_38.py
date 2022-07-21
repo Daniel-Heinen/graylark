4d51f2ed5163fef15c721214dc0e153fe7fd26db
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-07-21 10:58:24 +0000

    feat: add core functionality

--- src/geo_module_38.py
+++ src/geo_module_38.py
@@ -11,3 +11,4 @@ class LocationAnalyzer38:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2025-07-22
+# Modified 2022-07-21
