c47fa6eeeb3645367d9babeb5b943054135060eb
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-08-21 17:29:28 +0000

    security: add validation

--- src/geo_module_31.py
+++ src/geo_module_31.py
@@ -11,3 +11,4 @@ class LocationAnalyzer31:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2022-08-11
+# Modified 2022-08-21
