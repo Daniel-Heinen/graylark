a83a5f51680132091a81a11ed69c8a9d1253a988
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-09-12 10:29:09 +0000

    refactor: restructure codebase

--- src/geo_module_4.py
+++ src/geo_module_4.py
@@ -11,3 +11,4 @@ class LocationAnalyzer4:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2025-10-06
+# Modified 2022-09-12
