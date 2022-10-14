b96b00d2df7060aa2f0982fd0d4a7bd0416e6516
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-10-14 14:05:20 +0000

    test: improve coverage

--- src/geo_module_15.py
+++ src/geo_module_15.py
@@ -11,3 +11,4 @@ class LocationAnalyzer15:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2022-09-14
+# Modified 2022-10-14
