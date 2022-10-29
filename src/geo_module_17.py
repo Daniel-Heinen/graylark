3c21500eb6a4674702d68daa447f7072e13bf462
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-10-29 15:13:15 +0000

    fix: correct data validation

--- src/geo_module_17.py
+++ src/geo_module_17.py
@@ -11,3 +11,4 @@ class LocationAnalyzer17:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2025-08-22
+# Modified 2022-10-29
