c475a82abb14eb8878d6485106be9911ff568d6e
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-02-09 18:06:28 +0000

    fix: resolve authentication issue

--- src/geo_module_7.py
+++ src/geo_module_7.py
@@ -11,3 +11,4 @@ class LocationAnalyzer7:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2022-11-10
+# Modified 2023-02-09
