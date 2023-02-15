3736e7651abbc123dfea12ad24092a12b83728e7
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-02-15 11:55:11 +0000

    docs: update documentation

--- src/geo_module_23.py
+++ src/geo_module_23.py
@@ -11,3 +11,4 @@ class LocationAnalyzer23:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2022-09-27
+# Modified 2023-02-15
