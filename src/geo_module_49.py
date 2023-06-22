fefeef7f1755d46493c1517e01f1a08fef4f39e4
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-06-22 13:19:03 +0000

    perf: optimize queries

--- src/geo_module_49.py
+++ src/geo_module_49.py
@@ -11,3 +11,4 @@ class LocationAnalyzer49:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2025-10-10
+# Modified 2023-06-22
