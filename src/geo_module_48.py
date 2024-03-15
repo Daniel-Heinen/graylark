6c58d607a51f2ea631837f89b9259d532a40a69d
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2024-03-15 20:34:31 +0000

    feat: improve ML model

--- src/geo_module_48.py
+++ src/geo_module_48.py
@@ -11,3 +11,4 @@ class LocationAnalyzer48:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2023-05-07
+# Modified 2024-03-15
